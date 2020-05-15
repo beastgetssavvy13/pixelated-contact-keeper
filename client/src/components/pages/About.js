04b68b3fa446b4d48bb6caaab4ab095d32da9580
beastgetssavvy13 <kavish.pandit178@gmail.com>
2020-05-16 01:29:58 +0530

    ADD A IMAGE

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,17 +1,30 @@
 import React from 'react'
-
+import normal from './normal.jpeg';
 const About = () => {
-    return ( <div>
-        <h1 > About this app </h1> 
-        <p className = "my-1" >
-        This is a full stack React app
-        for keeping contacts </p> <p className = "bg-success p" >
-        <strong > Version: </strong>1.0.0 </p>
-        <p className="bg-dark p-2" >
-            <strong > Developer: </strong>Kavish Pandit<br />
-            <strong > Email: </strong>kavish.pandit178@gmail.com <br/>
-            <strong > Description: </strong> Created an application using Reat Hooks and Express Js Server. Feel free to reach out if any issues </p>  
+    return (<div>
+        <h1 > About this app </h1>
+        <p className="my-1" >
+            This is a full stack React app
+        for keeping contacts </p> <p className="bg-success p" >
+            <strong > Version: </strong>1.0.0 </p>
+        <div>
+            <p className="bg-dark p" >
+                <div class="square" >
+                    <img
+                        src={normal}
+                        style={{ width: '200px', margin: 'auto', display: 'round' }}
+                        alt='Loading...'
+                    /></div>
+                <strong >   Developer: </strong>Kavish Pandit<br />
+                <strong >   Email: </strong>kavish.pandit178@gmail.com <br />
+                <strong >   Technologies Used: </strong>VS STUDIO | POSTMAN | MONGODBATLAS | HEROKU <br />
+                <strong >   Github Link: </strong>https://github.com/beastgetssavvy13?tab=repositories <br />
+                <strong >   Description: </strong> Created an application using React Hooks and Express Js Server. Feel free to reach out if any issues
+            <br />
+            </p>
+
         </div>
+    </div>
     )
 }
 export default About
